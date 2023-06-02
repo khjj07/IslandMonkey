2df68f7fd54c9ca6af91490e43f583a9cd691d47
khjj07 <khjj07@gmail.com>
2023-06-03 01:42:48 +0900

    path creator 업로드 / 디버깅용 스크립트 작성

---
+++ Assets/IslandMonkey/Scripts/Debug/DebugSphereDrawer.cs
@@ -0,0 +1,33 @@
+using UnityEditor;
+using UnityEditor.PackageManager.UI;
+using UnityEngine;
+using static UnityEngine.Rendering.DebugUI.Table;
+
+namespace Assets.IslandMonkey.Scripts.Debug
+{
+#if UNITY_EDITOR
+    public class DebugSphereDrawer : MonoBehaviour
+    {
+
+        [SerializeField] private Color _color = Color.red;
+        [SerializeField] public float size = 1.0f;
+        [SerializeField] private bool _wireFrame = false;
+        // Update is called once per frame
+
+        void OnDrawGizmos()
+        {
+            Gizmos.color = _color;
+            if (_wireFrame)
+            {
+                Gizmos.DrawWireSphere(transform.position, size);
+            }
+            else
+            {
+                Gizmos.DrawSphere(transform.position, size);
+            }
+            
+        }
+    }
+#endif
+}
+
