4d5141fd29d8e988530411e2f5872674e6ee64d5
khjj07 <khjj07@gmail.com>
2023-09-21 10:34:45 +0900

    데이터 수정 / 배치시스템 기반

--- Assets/0_IslandMonkey/Scripts/Managers/GameManager.cs
+++ Assets/0_IslandMonkey/Scripts/Managers/GameManager.cs
@@ -16,8 +16,7 @@ public class GameManager : Singleton<GameManager>
     public int banana;
     public int gold;
     public int clam;
-    public Transform placeOrigin;
-    public Place placePrefab;
+
 
     public void Start()
     {
@@ -34,29 +33,10 @@ public class GameManager : Singleton<GameManager>
         {
             UIManager.instance.SetClam(clam);
         });
+    
     }
 
-    public void ShowPlacableArea()
-    {
-        List<Vector2Int> indexList = new List<Vector2Int>();
-        foreach (var b in BuildingManager.instance._buildings)
-        {
-            Vector2Int[] increasementList = { new Vector2Int(2, 0), new Vector2Int(1, -1), new Vector2Int(-1, -1), new Vector2Int(-2, 0), new Vector2Int(-1, 1), new Vector2Int(1, 1) };
-            foreach (var increasement in increasementList)
-            {
-                var newIndex = b.index + increasement;
-                var isPlaced = BuildingManager.instance._buildings.Find(_ =>
-                {
-                    return newIndex == _.index;
-                });
-                if (isPlaced !=null)
-                {
-                    indexList.Add(newIndex);
-                }
-            }
-        }
-
-    }
+   
 
     public void EarnGold(int income)
     {
--- Assets/0_IslandMonkey/Scripts/ScriptableObject/BuildingPurchaseData.cs
+++ Assets/0_IslandMonkey/Scripts/ScriptableObject/BuildingPurchaseData.cs
@@ -7,8 +7,6 @@ namespace Assets._0_IslandMonkey.Scripts.ScriptableObject
     [Serializable]
     public class BuildingPurchaseData
     {
-        [Header("유학 여부"), ReadOnly]
-        public bool isVoyage=false;
         [Header("시설 아이콘")]
         public Sprite iconImage;
 
@@ -26,12 +24,5 @@ namespace Assets._0_IslandMonkey.Scripts.ScriptableObject
 
         [Header("소요 시간")]
         public int duration;
-    }
-
-    [Serializable]
-    public class VoyageBuildingPurchaseData : BuildingPurchaseData
-    {
-        [Header("유학 여부"), ReadOnly]
-        public new bool isVoyage = true;
     }
 }
